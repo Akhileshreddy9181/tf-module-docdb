68bd0d795ba0228cf3c2e5db013b015c96415374
akhil <akhileshreddy9181@gmail.com>
2023-05-26 17:03:02 +0530

    Added Exit Status to script

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_docdb_cluster" "main" {
   cluster_identifier      = "${var.env}-docdb"
   engine                  = "docdb"
@@ -6,10 +7,10 @@ resource "aws_docdb_cluster" "main" {
   master_password         = data.aws_ssm_parameter.pass.value
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
-  skip_final_snapshot     = true
+  skip_final_snapshot     = var.skip_final_snapshot
   db_subnet_group_name = aws_docdb_subnet_group.main.name
   kms_key_id = data.aws_kms_key.key.arn
-  storage_encrypted = true
+  storage_encrypted = var.storage_encrypted
 }
 
 
