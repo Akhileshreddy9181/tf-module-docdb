eef3c058260a85b824d2443667103b1f3e6e6aa8
akhil <akhileshreddy9181@gmail.com>
2023-06-22 18:00:41 +0530

    Added Exit Status to script

--- vars.tf
+++ vars.tf
@@ -6,6 +6,8 @@ variable "skip_final_snapshot" {}
 variable "engine_version" {}
 variable "subnet_ids" {}
 variable "tags" {}
+variable "allow_subnets" {}
+variable "vpc_id" {}
 
 variable "storage_encrypted" {
   default = true
